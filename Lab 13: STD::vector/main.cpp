11422c4d14c106004dda10341144778794d7de91
Guo An Wang <you@example.com>
2025-09-19 21:31:16 -0700

    Initial commit

---
+++ Lab 13: STD::vector/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  COMSC-210 | Lab 13: Lab 12: STD::vector | Guo An Wang
+//  IDE used: Xcode
+//  Created by Guo An Wang on 9/19/25.
+//
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <numeric>
+#include <array>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
